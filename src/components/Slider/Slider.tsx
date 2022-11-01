094e9f496946347b60e305c116096d7acdfc1819
promius <bodiachernii@gmail.com>
2022-11-01 20:59:48 +0200

    changed library and redone slider

--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -1,117 +1,38 @@
-import * as React from 'react';
-import {useTheme} from '@mui/material/styles';
-import Box from '@mui/material/Box';
-
-import Typography from '@mui/material/Typography';
-import SwipeableViews from 'react-swipeable-views';
-import {autoPlay} from 'react-swipeable-views-utils';
-
-import {FC, useEffect, useState} from "react";
-import {moviesActions} from "../../redux";
+import React, {FC, useEffect} from 'react';
+import {Paper} from "@mui/material";
+import Carousel from 'react-material-ui-carousel';
 import {useAppDispatch, useAppSelector} from "../../hooks";
+import {moviesActions, moviesReducer} from "../../redux";
 
-
-const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
-
+import './Slider.scss'
 
 const Slider: FC = () => {
-  const theme = useTheme();
-  const [activeStep, setActiveStep] = useState<number>(0);
-
-
-  const dispatch = useAppDispatch()
 
   const {popular} = useAppSelector(state => state.moviesReducer)
-
-  const maxSteps = popular.length;
+  const dispatch = useAppDispatch()
 
   useEffect(() => {
     dispatch(moviesActions.getPopular())
   }, [dispatch])
 
-  const handleNext = () => {
-    setActiveStep((prevActiveStep) => prevActiveStep + 1);
-  };
-
-  const handleBack = () => {
-    setActiveStep((prevActiveStep) => prevActiveStep - 1);
-  };
-
-  const handleStepChange = (step: number) => {
-    setActiveStep(step);
-  };
-
   return (
-    <>
-      <h1 style={{
-        marginLeft: '8%'
-      }}>Popular Today</h1>
 
-      <Box sx={{
-        maxWidth: '1000px',
-        height: '500px',
-        position: 'relative',
-        flexGrow: 1,
-        width: '100%',
-        margin: '20px auto',
-        display: 'flex',
-        justifyContent: 'centre',
-      }
-      }>
+  <div className="coursel">
+    <h1 className={'coursel__title'}>Popular Today</h1>
+    <Carousel className={'slider'}>
 
-        <AutoPlaySwipeableViews
-          axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
-          index={activeStep}
-          onChangeIndex={handleStepChange}
-          enableMouseEvents
-        >
-          {popular.map((step, index) => (
-            <>
-              <Typography
-                sx={{
-                  width: '100%',
-                  height: '500px',
-                  display: 'flex',
-                  alignItems: 'flex-end',
-                  padding: '5%',
-                  background: 'rgba(0,0,0,0.6)',
-                  position: 'absolute',
-                  color: 'red',
-                  fontWeight: '900',
-                  fontSize: '30px',
+      {popular.map((item, i) => (
+        <Paper className={'slider__item'}>
+          <img src={`https://image.tmdb.org/t/p/original/${item.poster_path}`} alt="Movie"
+               className={'slider__item-image'}/>
+          <h2 className={'slider__item-name'}>{item.title}</h2>
+        </Paper>
+      ))}
+    </Carousel>
+  </div>
 
-                }}
-              ></Typography>
-              <Box
-                sx={{
-                  width: '100%',
-                }}
-              >
-
-              </Box>
-              <div key={step.title}>
-                {Math.abs(activeStep - index) <= 2 ? (
-                  <Box
-                    component="img"
-                    sx={{
-                      objectFit: 'cover',
-                      maxHeight: 500,
-                      display: 'block',
-                      maxWidth: '1000px',
-                      overflow: 'hidden',
-                      textAlign: 'centre',
-                      width: '100%',
-                    }}
-                    src={`https://image.tmdb.org/t/p/original/${step.poster_path}`}
-                  />
-                ) : null}
-              </div>
-            </>
-          ))}
-        </AutoPlaySwipeableViews>
-
-      </Box>
-    </>);
+  );
 }
 
-export {Slider};
+
+export {Slider}
--- src/pages/MoviesPage.tsx
+++ src/pages/MoviesPage.tsx
@@ -1,12 +1,11 @@
 import React, {FC} from 'react';
 
-import {Genres, Movies, Slider} from "../components";
+import {Genres, Movies, Slider,} from "../components";
 
 
 const MoviesPage: FC = () => {
   return (
     <div>
-
       <Slider/>
       <Genres/>
       <Movies/>
